89c4a96b3aa1616e2aea61e676568c1de9db64e5
Renan Marques <nan.dev01@gmail.com>
2023-12-02 16:20:57 -0300

    Implementa a função getAnimalsOlderThan

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,9 @@
 const data = require('../data/zoo_data');
 
 const getAnimalsOlderThan = (animal, age) => {
-  // seu código aqui
+  const species = data.species.find(({ name }) => animal === name);
+  const isOlderThan = species.residents.every((resident) => resident.age >= age);
+  return isOlderThan;
 };
 
 module.exports = getAnimalsOlderThan;
