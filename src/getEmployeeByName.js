24373f29cbe7b9a547f8f9db2e7a77d536ebf8ec
Renan Marques <nan.dev01@gmail.com>
2023-12-02 16:33:10 -0300

    Implementa a função getEmployeeByName

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,11 @@
 const data = require('../data/zoo_data');
 
 const getEmployeeByName = (employeeName) => {
-  // seu código aqui
+  if (!employeeName) return {};
+
+  const employeeInfo = data.employees
+    .find(({ firstName, lastName }) => firstName === employeeName || lastName === employeeName);
+  return employeeInfo;
 };
 
 module.exports = getEmployeeByName;
