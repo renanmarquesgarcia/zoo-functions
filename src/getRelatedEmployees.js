18b86b85d6131ce2ac4c912df64650ac7e8e5fe1
Renan Marques <nan.dev01@gmail.com>
2023-12-04 07:17:27 -0300

    Implementa as funções isManager e getRelatedEmployees

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,20 @@
 const data = require('../data/zoo_data');
 
 const isManager = (id) => {
-  // seu código aqui
+  const manager = data.employees.some(({ managers }) => managers.includes(id));
+  return manager;
 };
 
 const getRelatedEmployees = (managerId) => {
-  // seu código aqui
+  if (!isManager(managerId)) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+
+  const managedPeople = data.employees
+    .filter(({ managers }) => managers.includes(managerId))
+    .map(({ firstName, lastName }) => `${firstName} ${lastName}`);
+
+  return managedPeople;
 };
 
 module.exports = { isManager, getRelatedEmployees };
