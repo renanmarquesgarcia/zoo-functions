3617f8d254a5c4e42a038f8ff25896dad2388086
Renan Marques <nan.dev01@gmail.com>
2023-12-06 12:08:49 -0300

    Implementa a função getOldestFromFirstSpecies

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,17 @@
 const data = require('../data/zoo_data');
 
 const getOldestFromFirstSpecies = (id) => {
-  // seu código aqui
+  const getFirstSpecieId = data.employees
+    .find((employee) => employee.id === id)
+    .responsibleFor[0];
+
+  const specieListOrderedByAge = data.species
+    .find((specie) => specie.id === getFirstSpecieId)
+    .residents.sort((a, b) => a.age - b.age);
+
+  return Object.values(specieListOrderedByAge[specieListOrderedByAge.length - 1]);
 };
+
+getOldestFromFirstSpecies('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1');
 
 module.exports = getOldestFromFirstSpecies;
