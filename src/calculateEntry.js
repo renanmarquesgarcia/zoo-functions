36397a9bd72a48a19ea283d21d0e525be157e620
Renan Marques <nan.dev01@gmail.com>
2023-12-06 09:44:24 -0300

    Implementa a funções countEntrants e calculateEntry

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,24 @@
 const data = require('../data/zoo_data');
 
 const countEntrants = (entrants) => {
-  // seu código aqui
+  const entrantsByAge = entrants.reduce((acc, { age }) => {
+    if (age < 18) acc.child += 1;
+    else if (age < 50) acc.adult += 1;
+    else acc.senior += 1;
+
+    return acc;
+  }, { child: 0, adult: 0, senior: 0 });
+
+  return entrantsByAge;
 };
 
 const calculateEntry = (entrants) => {
-  // seu código aqui
+  if (!entrants || Object.keys(entrants).length === 0) return 0;
+  const { child, adult, senior } = countEntrants(entrants);
+  const { prices } = data;
+
+  const totalRevenue = (child * prices.child) + (adult * prices.adult) + (senior * prices.senior);
+  return Number(totalRevenue.toFixed(2));
 };
 
 module.exports = { calculateEntry, countEntrants };
