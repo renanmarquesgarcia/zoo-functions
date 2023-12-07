8802afa1a3b42a8008019852c58950997b035965
Renan Marques <nan.dev01@gmail.com>
2023-12-07 10:06:59 -0300

    Implementa a função countAnimals

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,21 @@
 const data = require('../data/zoo_data');
 
-const countAnimals = () => {
-  // seu código aqui
+const countAnimals = ({ species, sex } = {}) => {
+  const animals = data.species.reduce((acc, { name, residents }) => {
+    acc[name] = residents;
+    return acc;
+  }, {});
+
+  let result = animals;
+  if (!species || !sex) {
+    result = Object.entries(animals).reduce((acc, curr) => {
+      acc[curr[0]] = curr[1].length;
+      return acc;
+    }, {});
+  }
+  if (species) result = animals[species].length;
+  if (sex) result = animals[species].filter((animal) => animal.sex === sex).length;
+  return result;
 };
 
 module.exports = countAnimals;
