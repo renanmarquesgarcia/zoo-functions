5c05f4a9bd039557e11bd21102cbd5ae60f7c658
Renan Marques <nan.dev01@gmail.com>
2023-12-09 10:00:43 -0300

    Implementa a função getAnimalMap

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,23 @@
 const data = require('../data/zoo_data');
 
-const getAnimalMap = (options) => {
-  // seu código aqui
+const getAnimalMap = (options = {}) => {
+  const { includeNames, sorted, sex } = options;
+
+  const animalsByLocation = data.species.reduce((acc, { location }) => {
+    acc[location] = data.species
+      .filter((specie) => specie.location === location)
+      .map((specie) => {
+        if (!includeNames) return specie.name;
+        const residentsNames = specie.residents
+          .filter((resident) => (!sex || sex === resident.sex))
+          .map((resident) => resident.name);
+        if (sorted) residentsNames.sort();
+        return { [specie.name]: residentsNames };
+      });
+    return acc;
+  }, {});
+
+  return animalsByLocation;
 };
 
 module.exports = getAnimalMap;
